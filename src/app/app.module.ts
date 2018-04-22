771d114d3fd4678f7fe284032fa9770207d023cd
kimshuye <suntisuk.go@gmail.com>
2018-04-23 01:02:50 +0700

    material select input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
 
-// import { MatSelectModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material';
 // import 'hammerjs';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -46,7 +46,7 @@ import { UserDetailComponent } from './user-detail/user-detail.component';
     BrowserAnimationsModule,
     NoopAnimationsModule,
 
-    // MatSelectModule,
+    MatSelectModule,
 
     AppRoutingModule,
     HttpClientModule,
